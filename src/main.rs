69b7bb57ffd1ac982ec0f5146613ece5d38a08e9
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2023-12-03 16:07:17 -0300

    pre gui main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 mod traits;
 mod lib;
 
-use std::rc::Rc;
-
-use std::mem;
-
 use crate::traits::colors::color::{ColorTraits, ColorValue};
 use crate::traits::canvas::pixel::PixelTraits;
 use crate::traits::canvas::layer::LayerTraits;
@@ -19,11 +15,6 @@ fn main() {
 
     let col1 = colors::Color::<u8>::new([255, 0, 0, 1]);
 
-    println!("{}", std::mem::size_of::<
-            canvas::Pixel<
-                colors::Color<u8>
-            >
-    >());
     // let inserted = layer.add_color(col1);
 
     let mat: [[f64; 4]; 4] = [
@@ -41,9 +32,17 @@ fn main() {
 
     let default = colors::Color::<u8>::new([0, 0, 0, 0]);
     layer.drop_unused_colors();
-    layer.change_color_value(default, col1);
+    layer.change_color_value(default.clone(), col1.clone());
     println!("Cores da camada: {:?}", layer.get_colors());
 
+    use std::collections::HashMap;
+    let map = HashMap::from([
+        (default, vec![[0, 0], [0, 1]]),
+    ]);
+    layer.set_pixel_color(map);
+    println!("Cores da camada: {:?}", layer.get_colors());
+
+    let new_layer = layer.clone();
 
     println!("Hello, world!");
 }
