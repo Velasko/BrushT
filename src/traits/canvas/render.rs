1863f1d9a6d1ab33937102dbed80a6f37fef7ad3
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2023-12-05 02:28:23 -0300

    new render methods thought out

--- src/traits/canvas/render.rs
+++ src/traits/canvas/render.rs
@@ -9,6 +9,8 @@ where
 {
 	fn new(dimension: [usize; 2]) -> Self;
 	fn render(&mut self) -> &L;
+	fn insert(&mut self, index: usize, layer: L);
+	fn clear_cache(&mut self);
 }
 
 // how to add layers/masks ?
