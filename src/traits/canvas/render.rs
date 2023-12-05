baea8b6fc7782ca6d143995583997124565e01b7
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2023-12-05 02:37:02 -0300

    more methods. yet to change the inserted value type.

--- src/traits/canvas/render.rs
+++ src/traits/canvas/render.rs
@@ -9,8 +9,10 @@ where
 {
 	fn new(dimension: [usize; 2]) -> Self;
 	fn render(&mut self) -> &L;
-	fn insert(&mut self, index: usize, layer: L);
 	fn clear_cache(&mut self);
+
+	fn insert(&mut self, index: usize, layer: L);
+	fn pop(&mut self, layer: L);
 }
 
 // how to add layers/masks ?
